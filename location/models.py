ad2d479d05aee21b6f8c00b0120ef5d2a9c6531d
Flowmikro <cloplop091@gmail.com>
2023-05-23 18:34:15 +0300

    v1.1

--- location/models.py
+++ location/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+
+class Location(models.Model):
+    city = models.CharField(max_length=255)
+    state_name = models.CharField(max_length=255)
+    zip = models.PositiveIntegerField()
+    lat = models.DecimalField(max_digits=10, decimal_places=5)
+    lng = models.DecimalField(max_digits=10, decimal_places=5)
